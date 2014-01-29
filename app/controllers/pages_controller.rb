bdbc0904d3e25f7f2becac539b5243348ed1737e
leeduan <lee.duan1@gmail.com>
2014-01-28 21:17:53 -0800

    create /home route which directs to PagesController index action and index.html.haml view

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def index
+    @videos = Video.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Myflix::Application.routes.draw do
+  root to: 'pages#index'
+
   get 'ui(/:action)', controller: 'ui'
+  get 'home', to: 'pages#index'
+
+
 end
