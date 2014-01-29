d1f5a49ed04a08bf335d18ee5e2dc873f75250e4
leeduan <lee.duan1@gmail.com>
2014-01-28 21:16:25 -0800

    create videos table and migration; seed fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(
+  title: 'South Park',
+  description: 'An American adult animated sitcom created by Trey Parker and Matt Stone for the Comedy Central television network.',
+  small_cover_url: 'south_park',
+  large_cover_url: 'monk_large'
+)
+
+Video.create(
+  title: 'Futurama',
+  description: 'An American adult animated science fiction sitcom created by Matt Groening and developed by Groening and David X. Cohen for the Fox Broadcasting Company.',
+  small_cover_url: 'futurama',
+  large_cover_url: 'monk_large'
+)
+
+Video.create(
+  title: 'Monk',
+  description: 'An American adult animated science fiction sitcom created by Matt Groening and developed by Groening and David X. Cohen for the Fox Broadcasting Company.',
+  small_cover_url: 'monk',
+  large_cover_url: 'monk_large'
+)
+
+Video.create(
+  title: 'Family Guy',
+  description: 'An American adult animated sitcom created by Seth MacFarlane for the Fox Broadcasting Company.',
+  small_cover_url: 'family_guy',
+  large_cover_url: 'monk_large'
+)
