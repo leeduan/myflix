c11eadb3180ae419c9efed01775c2e5a9d7ce639
leeduan <lee.duan1@gmail.com>
2014-02-02 19:50:21 -0800

    change find_by video model test to last method

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,6 +4,6 @@ describe Video do
   it "saves itself" do
     video = Video.new(title: 'The Simpsons', description: 'An American adult animated sitcom created by Matt Groening for the Fox Broadcasting Company.', small_cover_url: 'south_park', large_cover_url: 'monk_large', category_id: '1')
     video.save
-    Video.find_by(title: 'The Simpsons').title.should == 'The Simpsons'
+    Video.last.title.should == 'The Simpsons'
   end
 end
