48139cc8afd4b75c7b6e19ffe4866f9e1521a1db
leeduan <lee.duan1@gmail.com>
2014-02-03 17:36:24 -0800

    implement search_by_title video class method

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,32 @@ describe Video do
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+
+  describe 'search_by_title' do
+    it 'returns an array of all matches ordered by created_at' do
+      futurama = Video.create(title: 'Futurama', description: 'A great show!')
+      back_to_future = Video.create(title: 'Back to the Future', description: 'An even better show!')
+      expect(Video.search_by_title('Futur')).to eq([back_to_future, futurama])
+    end
+    it 'returns an array of one video for a partial match' do
+      futurama = Video.create(title: 'Futurama', description: 'A great show!')
+      back_to_future = Video.create(title: 'Back to the Future', description: 'An even better show!')
+      expect(Video.search_by_title('urama')).to eq([futurama])
+    end
+    it 'returns an array of one video for an exact match' do
+      futurama = Video.create(title: 'Futurama', description: 'A great show!')
+      back_to_future = Video.create(title: 'Back to the Future', description: 'An even better show!')
+      expect(Video.search_by_title('Futurama')).to eq([futurama])
+    end
+    it 'returns an empty array if there is no match' do
+      futurama = Video.create(title: 'Futurama', description: 'A great show!')
+      back_to_future = Video.create(title: 'Back to the Future', description: 'An even better show!')
+      expect(Video.search_by_title('Hello')).to eq([])
+    end
+    it 'returns an empty array for a search with an empty string' do
+      futurama = Video.create(title: 'Futurama', description: 'A great show!')
+      back_to_future = Video.create(title: 'Back to the Future', description: 'An even better show!')
+      expect(Video.search_by_title('')).to eq([])
+    end
+  end
 end
