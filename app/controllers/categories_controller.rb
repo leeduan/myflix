7fd5bc8dd0d7014f1b7f23be2b11eca2a551522e
leeduan <lee.duan1@gmail.com>
2014-02-04 20:00:31 -0800

    move /home from videos controller to categories controller#index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
 
+  def index
+    @categories = Category.all
+  end
+
   def show; end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Myflix::Application.routes.draw do
-  root to: 'videos#index'
+  root to: 'categories#index'
 
   get 'ui(/:action)', controller: 'ui'
-  get :home, to: 'videos#index'
+  get :home, to: 'categories#index'
 
   resources :videos, only: [:show] do
     post :search, on: :collection
