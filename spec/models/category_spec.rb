0b6d00f54646f5182ce652f60a7b3d0b1a74eafa
leeduan <lee.duan1@gmail.com>
2014-02-08 14:09:17 -0800

    refactor video and category unit tests using context, subject and let

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,33 +5,43 @@ describe Category do
   it { should validate_presence_of(:name) }
 
   describe '#recent_videos' do
-    it 'returns the videos in the reverse chronological order by created at' do
-      action    = Category.create(name: 'action')
-      star_wars = action.videos.create(title: 'Star Wars', description: 'Great video!')
-      star_trek = action.videos.create(title: 'Star Trek', description: 'Great video!', created_at: 1.day.ago)
-      rock_star = action.videos.create(title: 'Thor', description: 'Great video!', created_at: 2.day.ago)
-      expect(action.recent_videos).to eq([star_wars, star_trek, rock_star])
+    let(:category) { Category.create(name: 'action') }
+    let(:should) { category.recent_videos }
+
+    context 'videos descending created at order' do
+      before do
+        @star_wars = category.videos.create(title: 'Star Wars', description: 'Great video!')
+        @star_trek = category.videos.create(title: 'Star Trek', description: 'Great video!', created_at: 1.day.ago)
+        @rock_star = category.videos.create(title: 'Thor',      description: 'Great video!', created_at: 2.day.ago)
+      end
+      it { should == [@star_wars, @star_trek, @rock_star] }
     end
-    it 'returns an empty array if the category does not have any videos' do
-      action    = Category.create(name: 'action')
-      expect(action.recent_videos).to eq([])
+
+    context 'no videos' do
+      it { should == [] }
     end
-    it 'returns all videos if total is less than six' do
-      action    = Category.create(name: 'action')
-      star_wars = action.videos.create(title: 'Star Wars', description: 'Great video!')
-      star_trek = action.videos.create(title: 'Star Trek', description: 'Great video!')
-      expect(action.recent_videos.count).to eq(2)
+
+    context 'less than six videos' do
+      before do
+        2.times { |i| category.videos.create(title: "Star Wars #{i}", description: 'Great video!') }
+      end
+      it { should.count == 2 }
     end
-    it 'returns 6 videos if there are more than 6 videos' do
-      action    = Category.create(name: 'action')
-      7.times { |i| action.videos << Video.create(title: "vid_#{i}", description: 'Great video!') }
-      expect(action.recent_videos.count).to eq(6)
+
+    context 'multiple videos' do
+      before do
+        6.times { |i| category.videos.create(title: "Star Wars #{i}", description: 'Great video!') }
+      end
+      it { should.count == 6 }
     end
-    it 'returns the most recent 6 videos' do
-      action    = Category.create(name: 'action')
-      6.times { |i| action.videos << Video.create(title: "vid_#{i}", description: 'Great video!') }
-      yesterday = action.videos << Video.create(title: "Yesterday", created_at: 1.day.ago)
-      expect(action.recent_videos).not_to include(yesterday)
+
+    context 'more than six videos' do
+      before do
+        6.times { |i| category.videos.create(title: "Star Wars #{i}", description: 'Great video!') }
+        @first_video = category.videos.create(title: "Yesterday", created_at: 1.day.ago)
+      end
+      it { should.count == 6 }
+      it { should.should_not == @first_video}
     end
   end
 end
