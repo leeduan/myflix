6826809355f4485a6b55a8e31acd924bb44b52c5
leeduan <lee.duan1@gmail.com>
2014-03-03 20:19:44 -0800

    change smtp settings to heroku mailgun addon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,17 +18,16 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {
     host: 'ld-myflix.herokuapp.com'
   }
   config.action_mailer.smtp_settings = {
-    address:              "smtp.gmail.com",
-    port:                 "587",
-    domain:               "gmail.com",
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       "plain",
-    enable_starttls_auto: true
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:               'ld-myflix.heroku.com',
+    authentication:       :plain,
   }
+  config.action_mailer.delivery_method = :smtp
 end
