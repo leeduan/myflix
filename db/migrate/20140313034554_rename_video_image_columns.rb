fb70a88d9a57d6cdeadb12d7af71c489fdfac9a3
Lee Duan <leeduan@Lees-MacBook-Pro.local>
2014-04-03 20:24:06 -0700

    upgrade gems, remove unneeded migration file

--- db/migrate/20140313034554_rename_video_image_columns.rb
+++
@@ -1,6 +0,0 @@
-class RenameVideoImageColumns < ActiveRecord::Migration
-  def change
-    rename_column :videos, :large_cover_url, :large_cover
-    rename_column :videos, :small_cover_url, :small_cover
-  end
-end
